01998adade24172b67bb6240466e0759f90e7a82
zyc19961009 <915732337@qq.com>
2020-08-18 23:18:40 +0800

    新剑法

---
+++ src/main/java/com/atguigu/jianfa/NewJianfa.java
@@ -0,0 +1,16 @@
+package com.atguigu.jianfa;
+
+public class NewJianfa {
+
+    public static void main(String[] args) {
+
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+        System.out.println("夺命三仙剑");
+
+    }
+}
