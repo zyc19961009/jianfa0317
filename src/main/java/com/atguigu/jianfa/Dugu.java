39a1cc3952f34b5ef533b159f01bd81a45689ed1
zyc <915732337@qq.com>
2020-08-18 22:08:18 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/jianfa/Dugu.java
@@ -0,0 +1,12 @@
+package com.atguigu.jianfa;
+
+public class Dugu {
+
+    public static void main(String[] args) {
+
+        System.out.println("独孤剑法好");
+        System.out.println("独孤剑法好");
+        System.out.println("独孤剑法好");
+
+    }
+}
